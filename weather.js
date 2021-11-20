073ccb28ef8fa7af3bd518f30142b1988abfeb3d
shon-arch <sunnywahane14@gmail.com>
2021-11-20 18:42:26 +0530

    Fixed the issue with the weird sunset and sunrise time Now displaying the weather forcast for next 24 hours

--- weather.js
+++ weather.js
@@ -49,22 +49,84 @@ async function getWeather()
 
     content.classList.remove("d-flex", "justify-content-center", "align-items-center")
 
+    create_main_info(json, cityName); 
     create_info(json);    
 
     content.removeChild(loader)
     
 }
 
+
+function create_main_info(json, cityName){
+    const generic_info = document.createElement("div"); 
+    generic_info.classList.add("generic-info"); 
+
+    const weather_overview = document.createElement("div"); 
+    weather_overview.classList.add("weather-overview", "p-4"); 
+
+    const h1 = document.createElement("h1"); 
+    h1.innerText = cityName[0].toUpperCase() + cityName.slice(1);  
+    const h3 = document.createElement("h3"); 
+    h3.innerText = json["current"]["weather"][0]["main"]; 
+    const p = document.createElement("p"); 
+    p.classList.add("main-temp");
+    p.innerText = json["current"]["temp"] + "°C"; 
+    weather_overview.appendChild(h1); 
+    weather_overview.appendChild(h3); 
+    weather_overview.appendChild(p); 
+
+
+    const hourly_info = document.createElement("div"); 
+    hourly_info.classList.add("two-four-weather"); 
+
+    for(let i = 0 ;i<24; ++i){
+        hourly_info.appendChild(create_hour_divs(json["hourly"][i])); 
+        // console.log(hour_info); 
+    }
+
+
+    generic_info.appendChild(weather_overview); 
+    generic_info.appendChild(hourly_info); 
+    content.appendChild(generic_info); 
+  
+   
+
+
+}
+
+function create_hour_divs(hour_info){ 
+    const div = document.createElement("div"); 
+
+    div.classList.add("hour-info")
+
+    const h4 = document.createElement("h4"); 
+    const img = document.createElement("img"); 
+    const p = document.createElement("p"); 
+
+    h4.innerText = (new Date(hour_info["dt"]*1000)).getHours(); 
+    img.setAttribute("src", "http://openweathermap.org/img/wn/" + hour_info["weather"][0]["icon"] +"@2x.png");
+    p.innerText = hour_info["temp"] +"°C"; 
+
+    div.appendChild(h4); 
+    div.appendChild(img); 
+    div.appendChild(p); 
+    return div; 
+
+}
+
+
+
+
+
 function create_info(json){
 
     const weather_table = document.createElement("div"); 
-    weather_table.classList.add("middle-info")
+    weather_table.classList.add("middle-info", "p-5")
 
-    //To do : Look into the sunset and sunrise time and veryify whether they are correct
-    let sunrise_time = new Date(json["current"]["sunrise"])
+    let sunrise_time = new Date(json["current"]["sunrise"]*1000)
     sunrise_time = sunrise_time.toLocaleTimeString()
 
-    let sunset_time = new Date(json["current"]["sunset"])
+    let sunset_time = new Date(json["current"]["sunset"]*1000)
     sunset_time = sunset_time.toLocaleTimeString();  
 
     weather_table.appendChild(create_td("Sunrise",sunrise_time ))
@@ -84,6 +146,7 @@ function create_info(json){
 
 function create_td(name, value, unit=""){
     let div = document.createElement("td");
+    div.classList.add("general-weather-spec"); 
     div.style.width = "350px"
     let h5 = document.createElement("h5"); 
     h5.innerText = name; 
