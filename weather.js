92876381cee362093972344b59c7188876dceaea
shon-arch <sunnywahane14@gmail.com>
2021-11-20 14:08:30 +0530

    Added function to read generic weather data according to location and displaying them using a table

--- weather.js
+++ weather.js
@@ -1,6 +1,5 @@
 var key = config.SECRET_API_KEY; 
 
-
 function sumbitName(ev){
     ev.preventDefault(); 
     getWeather()
@@ -9,6 +8,8 @@ function sumbitName(ev){
 const loader = document.createElement("div");
 loader.classList.add("spinner-border", "spinner")
 
+const content = document.querySelector('.content'); 
+
 document.getElementById("search").addEventListener("click", sumbitName) 
 
 async function getWeather() 
@@ -16,7 +17,8 @@ async function getWeather()
     city = document.getElementById("city-name")
     cityName = (city.value).toLowerCase()
 
-    const content = document.querySelector('.content'); 
+    content.innerHTML = ""; 
+
     content.classList.add("d-flex", "justify-content-center", "align-items-center")
     
     content.appendChild(loader)
@@ -38,7 +40,7 @@ async function getWeather()
     let lon = location_data["coord"]["lon"]; 
 
     detailed_response = await fetch(
-    "https://api.openweathermap.org/data/2.5/onecall?lat="+lat +"&lon="+lon+"&appid=" +key
+    "https://api.openweathermap.org/data/2.5/onecall?lat="+lat +"&lon="+lon+"&units=metric" + "&appid=" +key
     );
 
     let json = await detailed_response.json(); 
@@ -47,35 +49,56 @@ async function getWeather()
 
     content.classList.remove("d-flex", "justify-content-center", "align-items-center")
 
+    create_info(json);    
+
+    content.removeChild(loader)
+    
+}
+
+function create_info(json){
+
     const weather_table = document.createElement("table"); 
     weather_table.classList.add("table", "table-dark")
     const row1 = document.createElement("tr")
     const row2 = document.createElement("tr")
 
-    row1.appendChild(create_td("sunrise", json["current"]["sunrise"]))
-    row1.appendChild(create_td("sunset", json["current"]["sunset"]))
-    row1.appendChild(create_td("wind_speed", json["current"]["wind_speed"]))
+
+    //To do : Look into the sunset and sunrise time and veryify whether they are correct
+    let sunrise_time = new Date(json["current"]["sunrise"])
+    sunrise_time = sunrise_time.toLocaleTimeString()
+
+    let sunset_time = new Date(json["current"]["sunset"])
+    sunset_time = sunset_time.toLocaleTimeString();  
+
+    row1.appendChild(create_td("Sunrise",sunrise_time ))
+    row1.appendChild(create_td("Sunset", sunset_time))
+    row1.appendChild(create_td("Wind Speed", json["current"]["wind_speed"], "m/s"))
+    row1.appendChild(create_td("Precipitation", json["hourly"][0]["pop"]*100, "%"))
+    row1.appendChild(create_td("Atmospheric Temperature", json["current"]["dew_point"],"°C"))
+
+
+    row2.appendChild(create_td("Pressure", json["current"]["pressure"], "hPa"))
+    row2.appendChild(create_td("Humidity", json["current"]["humidity"], "%"))
+    row2.appendChild(create_td("Visibility", json["current"]["visibility"]/1000, "KM"))
+    row2.appendChild(create_td("Feels Like", json["current"]["feels_like"], "°C"))
+    row2.appendChild(create_td("Uvi Index", json["current"]["uvi"]))
+
 
     weather_table.appendChild(row1);
+    weather_table.appendChild(row2); 
     content.appendChild(weather_table); 
-
-   content.removeChild(loader)
-    
 }
 
-function create_td(name, value){
+
+function create_td(name, value, unit=""){
     let td = document.createElement("td");
     let h5 = document.createElement("h5"); 
     h5.innerText = name; 
     let p = document.createElement("p"); 
-    p.innerText=value; 
+    p.innerText= (value+" " + unit); 
     td.appendChild(h5); 
     td.appendChild(p); 
     return td;  
 }
   
-
-
-
-
-getWeather()
+getWeather(); 
